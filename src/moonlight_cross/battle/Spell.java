5e1db384a9bd2e7f4243b5de2d4c6a4ce98754b4
Rafael Dias <persephius@protonmail.com>
2022-05-06 12:21:54 -0300

    Preparing systems for spell usage

--- src/moonlight_cross/battle/Spell.java
+++ src/moonlight_cross/battle/Spell.java
@@ -3,19 +3,14 @@ package moonlight_cross.battle;
 public abstract class Spell {
     protected String name;
     protected String description;
-    protected Type type;
     protected int cost;
-
-    public abstract void cast(BattleEntity target);
+    protected Effect effect;
 
     public String get_name(){
         return name;
     }
     public String get_description(){
         return description;
-    }
-    public Type get_type(){
-        return type;
     }
     public int get_cost(){
         return cost;
@@ -27,9 +22,6 @@ public abstract class Spell {
     public void set_description(String new_description){
         description = new_description;
     }
-    public void set_type(Type new_type){
-        type = new_type;
-    }
     public void set_cost(int new_cost){
         cost = new_cost;
     }
@@ -39,23 +31,68 @@ public abstract class Spell {
 enum Type {
     FIRE,
     WATER,
-    WIND
+    WIND,
+    NEUTRAL
 }
 
-class Fireball extends Spell {
-    int damage;
-    public Fireball (){
-        name = "Fireball";
-        description = "The most basic of all offensive spells!";
-        type = Type.FIRE;
-        cost = 5;
-        damage = 10;
+// Effect classes
+
+// Exists to avoid repeated code
+abstract class Effect {
+    protected boolean is_overtime = false;
+    protected Type element = Type.NEUTRAL;
+    protected int amount = 0;
+
+    // Cast is declared here and not in the Spell class because if
+    // a spell doesn't have an active effect in battle, it makes no
+    // sense to allow it to be cast. If it does something else, like
+    // a key spell to a certain part of the game, then it's not a default
+    // spell, same as key items would work, they're exceptions!
+    public abstract void Cast(BattleEntity target);
+
+    public Type getElement() {
+        return element;
+    }
+
+    public void setElement(Type element) {
+        this.element = element;
+    }
+}
+
+class DamageEffect extends Effect {
+    public DamageEffect(int damage, Type element, boolean is_overtime) {
+        this.amount = damage;
+        this.element = element;
+        this.is_overtime = is_overtime;
     }
 
     @Override
-    public void cast(BattleEntity target) {
-        System.out.println("A " + name + " has been casted.");
-        target.takeDamage(damage);
+    public void Cast(BattleEntity target) {
+        target.takeDamage(amount);
     }
+}
 
+class HealingEffect extends Effect {
+    @Override
+    public void Cast(BattleEntity target){
+
+    }
+}
+
+class Fireball extends Spell {
+    public Fireball (){
+        name = "Fireball";
+        description = "The most basic of all offensive spells!";
+        cost = 5;
+        effect = new DamageEffect(5, Type.FIRE, false);
+    }
+}
+
+class Blizzard extends Spell {
+    public Blizzard (){
+        name = "Blizzard";
+        description = "Sends shards of crystallized ice at high velocity.";
+        cost = 5;
+        effect = new DamageEffect(5, Type.WATER, false);
+    }
 }
