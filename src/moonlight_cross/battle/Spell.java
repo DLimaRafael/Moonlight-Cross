ca0fe6e4d4db301d70c34bc4e13a86d3f2e813c5
Rafael Dias <persephius@protonmail.com>
2022-06-04 13:42:06 -0300

    Changes for spell usage

--- src/moonlight_cross/battle/Spell.java
+++ src/moonlight_cross/battle/Spell.java
@@ -1,92 +1,70 @@
 package moonlight_cross.battle;
 
+import moonlight_cross.battle.components.SpellEffect;
+
 public abstract class Spell {
     protected String name;
     protected String description;
     protected int cost;
-    protected Effect effect;
+    protected SpellEffect effect;
 
-    public String get_name(){
+    protected enum Type {
+        FIRE,
+        WATER,
+        WIND,
+        NEUTRAL
+    }
+    
+    protected Type element = Type.NEUTRAL;
+
+    public String getName(){
         return name;
     }
-    public String get_description(){
+    public String getDescription(){
         return description;
     }
-    public int get_cost(){
+    public int getCost(){
         return cost;
     }
 
-    public void set_name(String new_name){
+    public void setName(String new_name){
         name = new_name;
     }
-    public void set_description(String new_description){
+    public void setDescription(String new_description){
         description = new_description;
     }
-    public void set_cost(int new_cost){
+    public void setCost(int new_cost){
         cost = new_cost;
     }
-}
-
-// Enum for damage classes
-enum Type {
-    FIRE,
-    WATER,
-    WIND,
-    NEUTRAL
-}
-
-// Effect classes
-
-// Exists to avoid repeated code
-abstract class Effect {
-    protected boolean is_overtime = false;
-    protected Type element = Type.NEUTRAL;
-
-    // Amount refers to the effect intensity.
-    protected int amount = 0;
-
-    public Effect(boolean is_overtime, Type element, int amount) {
-        this.is_overtime = is_overtime;
-        this.element = element;
-        this.amount = amount;
-    }
-
-    // Cast is declared here and not in the Spell class because if
-    // a spell doesn't have an active effect in battle, it makes no
-    // sense to allow it to be cast. If it does something else, like
-    // a key spell to a certain part of the game, then it's not a default
-    // spell, same as key items would work, they're exceptions!
-    public abstract void Cast(BattleEntity target);
 
     public Type getElement() {
         return element;
     }
-
     public void setElement(Type element) {
         this.element = element;
     }
 }
 
-class DamageEffect extends Effect {
-    public DamageEffect(boolean is_overtime, Type element, int amount) {
-        super(is_overtime, element, amount);
+class DamageEffect extends SpellEffect {
+    public DamageEffect(boolean is_overtime, int potency) {
+        super(is_overtime, potency);
     }
 
     @Override
-    public void Cast(BattleEntity target) {
-        target.takeDamage(amount);
+    public void cast(BattleEntity target) {
+        target.takeDamage(potency);
     }
 }
 
-class HealingEffect extends Effect {
-    public HealingEffect(boolean is_overtime, Type element, int amount) {
-        super(is_overtime, element, amount);
+class HealingEffect extends SpellEffect {
+    public HealingEffect(boolean is_overtime, int amount) {
+        super(is_overtime, amount);
     }
 
     @Override
-    public void Cast(BattleEntity target){
-        target.setHp(target.getHp() + amount);
-        System.out.println(target.NAME + " was healed by " + amount + " HP!");
+    public void cast(BattleEntity target){
+        target.setHp(target.getHp() + potency);
+        System.out.println(target.NAME + " was healed by " + potency + " HP!");
     }
 }
 
@@ -95,7 +73,8 @@ class Fireball extends Spell {
         name = "Fireball";
         description = "The most basic of all offensive spells!";
         cost = 5;
-        effect = new DamageEffect(false, Type.FIRE, 5);
+        element = Type.FIRE;
+        effect = new DamageEffect(false, 5);
     }
 }
 
@@ -104,6 +83,7 @@ class Blizzard extends Spell {
         name = "Blizzard";
         description = "Sends shards of crystallized ice at high velocity.";
         cost = 5;
-        effect = new DamageEffect(false, Type.WATER, 5);
+        element = Type.WATER;
+        effect = new DamageEffect(false, 5);
     }
 }
---
+++ src/moonlight_cross/battle/components/SpellHandler.java
@@ -0,0 +1,5 @@
+package moonlight_cross.battle.components;
+
+public class SpellHandler {
+    
+}
