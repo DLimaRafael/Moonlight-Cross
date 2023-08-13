161f697f71487790fb09a8727827e115b4215d78
Rafael D. Lima <86539312+DLimaRafael@users.noreply.github.com>
2023-08-13 14:41:48 -0300

    Update build file, remove ItemEffect file

--- build.bat
+++ build.bat
@@ -4,5 +4,6 @@ REM === FOR REMOVING UNDESIRED .class files:
 REM for /r "src" %%f in (*.class) do del "%%f"
 
 REM === ENTERS THE src FOLDER AND COMPILES IT TO THE bin FOLDER:
+REM use it from outside of src as just ".\build.bat"
 javac -cp "src" -d "bin" "src/engine/Main.java"
 java -cp bin engine.Main
